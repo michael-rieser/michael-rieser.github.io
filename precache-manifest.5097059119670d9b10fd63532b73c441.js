8f842440f40b75472a26b54cf1ab6ff791c78629
Michael F. Rieser <rieser.20@osu.edu>
2020-05-25 09:08:58 -0400

    build update

---
+++ precache-manifest.5097059119670d9b10fd63532b73c441.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a5334482c99f04ff628e38a8aa6756",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1b680767b1da543a7a3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9ce000ee2552a1823142",
+    "url": "/static/js/2.ba0ead68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba0ead68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b680767b1da543a7a3",
+    "url": "/static/js/main.60fb70fe.chunk.js"
+  },
+  {
+    "revision": "80e274ec0ec388e5e202",
+    "url": "/static/js/runtime-main.c8b434d9.js"
+  }
+]);
